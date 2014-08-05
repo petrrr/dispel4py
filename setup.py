14cf8fca00dc3267ddc6356a6a8199e87d2a492a
Peter Danecek <Peter.Danecek@bo.ingv.it>
2014-08-05 19:22:39 +0200

    PEP 8: setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,24 @@ from setuptools import setup
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "dispel4py",
-    version = "0.0.1",
-    author = "The University of Edinburgh",
-    author_email = "a.krause@epcc.ed.ac.uk",
-    description = ("Dispel4py is a Python library used to describe abstract workflows for distributed data-intensive applications."),
+    name="dispel4py",
+    version="0.0.1",
+    author="The University of Edinburgh",
+    author_email="a.krause@epcc.ed.ac.uk",
+    description=(
+        "Dispel4py is a Python library used to describe abstract workflows \
+        for distributed data-intensive applications."),
     license = "Apache 2",
     keywords = "dispel4py dispel workflows processing elements",
     url = "https://github.com/akrause2014/dispel4py",
-    packages=['dispel4py', 'dispel4py.seismo', 'dispel4py.storm', 'test', 'test.graph_testing'],
+    packages=['dispel4py', 'dispel4py.seismo', 'dispel4py.storm',
+              'test', 'test.graph_testing'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
